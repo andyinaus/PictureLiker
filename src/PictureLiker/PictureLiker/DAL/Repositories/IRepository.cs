510ab585468eaa51c6a820d1fa0fd0364782c4a0
Andy <andy.hsu.job@gmail.com>
2019-03-28 21:38:48 +1000

    Added GenericRepo

---
+++ src/PictureLiker/PictureLiker/DAL/Repositories/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace PictureLiker.DAL.Repositories
+{
+    public interface IRepository<T> where T : EntityBase
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> ListAsync();
+        Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        void Delete(T entity);
+        void Edit(T entity);
+    }
+
+    public abstract class EntityBase
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public int Id { get; protected set; }
+    }
+}
---
+++ src/PictureLiker/PictureLiker/DAL/Repositories/Repository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PictureLiker.DAL.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : EntityBase
+    {
+        private readonly PictureLikerContext _dbContext;
+
+        public Repository(PictureLikerContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> ListAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbContext.Set<T>()
+                .Where(predicate)
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Remove(entity);
+        }
+
+        public void Edit(T entity)
+        {
+            _dbContext.Update(entity);
+        }
+    }
+}
