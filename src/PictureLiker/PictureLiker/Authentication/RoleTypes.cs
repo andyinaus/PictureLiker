b1519e012b18575b99eb52554934b3bd8640f47f
Andy <andy.hsu.job@gmail.com>
2019-03-29 08:11:42 +1000

    Added RoleTypes for authentication purpose

---
+++ src/PictureLiker/PictureLiker/Authentication/RoleTypes.cs
@@ -0,0 +1,8 @@
+﻿namespace PictureLiker.Authentication
+{
+    public static class RoleTypes
+    {
+        public const string Administrator = "Administrator";
+        public const string GeneralUser = "GeneralUser";
+    }
+}
