bbea6e9fec9099147e53b3603956c2fa90ab618a
Andy <andy.hsu.job@gmail.com>
2019-03-28 12:59:14 +1000

    Added Login page and its controller

---
+++ src/PictureLiker/PictureLiker/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PictureLiker.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
