7940e380e58695ffb59289da6c9ce72c1d841515
Andy <andy.hsu.job@gmail.com>
2019-03-28 13:31:41 +1000

    Added post action for Sign in

--- src/PictureLiker/PictureLiker/Controllers/AccountController.cs
+++ src/PictureLiker/PictureLiker/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PictureLiker.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -6,9 +8,22 @@ namespace PictureLiker.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (model == null) throw new ArgumentNullException(nameof(model));
+
+            if (!ModelState.IsValid) return View(model);
+            
+            //todo: save user info to db
+
+            return View(model);
         }
     }
 }
