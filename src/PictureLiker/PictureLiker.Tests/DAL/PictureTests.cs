e22d4bc39d382f0654210d2a5076d29d75b48b01
Andy <andy.hsu.job@gmail.com>
2019-03-29 09:46:50 +1000

    Added Picture and its unit tests

---
+++ src/PictureLiker/PictureLiker.Tests/DAL/PictureTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using PictureLiker.DAL;
+using Xunit;
+
+namespace PictureLiker.Tests.DAL
+{
+    public class PictureTests
+    {
+        [Fact]
+        public void SetBytesWithNullBytesShouldThrowArgumentNullException()
+        {
+            var picture = new Picture();
+
+            Action action = () => picture.SetBytes(null);
+
+            Assert.Throws<ArgumentNullException>(action);
+        }
+
+        [Fact]
+        public void SetBytesWithValidBytesShouldSetCorrectly()
+        {
+            var picture = new Picture();
+            var validBytes = new byte[10];
+            new Random().NextBytes(validBytes);
+
+            picture.SetBytes(validBytes);
+
+            Assert.Equal(validBytes, picture.Bytes);
+        }
+    }
+}
---
+++ src/PictureLiker/PictureLiker/DAL/Picture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using PictureLiker.DAL.Repositories;
+
+namespace PictureLiker.DAL
+{
+    public class Picture : EntityBase
+    {
+        public Picture SetBytes(byte[] bytes)
+        {
+            Bytes = bytes ?? throw new ArgumentNullException(nameof(bytes));
+
+            return this;
+        }
+
+        public byte[] Bytes { get; private set; }
+    }
+}
