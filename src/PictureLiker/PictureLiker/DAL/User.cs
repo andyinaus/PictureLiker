2700bdc5938a7d3892b952ece03f23ccc283c4c2
Andy <andy.hsu.job@gmail.com>
2019-03-28 15:44:14 +1000

    Added User entity

---
+++ src/PictureLiker/PictureLiker/DAL/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PictureLiker.DAL
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Email { get; private set; }
+    }
+}
