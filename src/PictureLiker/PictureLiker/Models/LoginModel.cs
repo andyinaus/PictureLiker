edbfc58bf28adfe152674a161c4d374986468747
Andy <andy.hsu.job@gmail.com>
2019-03-28 13:30:52 +1000

    Added DataAnnotation for LoginModel

--- src/PictureLiker/PictureLiker/Models/LoginModel.cs
+++ src/PictureLiker/PictureLiker/Models/LoginModel.cs
@@ -1,9 +1,14 @@
-﻿namespace PictureLiker.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PictureLiker.Models
 {
     public class LoginModel
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
         public string PreferredName { get; set; }
     }
 }
