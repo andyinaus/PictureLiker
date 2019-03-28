408c94cdb12b22c112dca56d75cad1bc5a4f9890
Andy <andy.hsu.job@gmail.com>
2019-03-28 18:22:14 +1000

    Re-wired DB connection

--- src/PictureLiker/PictureLiker/Startup.cs
+++ src/PictureLiker/PictureLiker/Startup.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PictureLiker.DAL;
 
 namespace PictureLiker
 {
     public class Startup
     {
+        private const string DefaultConnectionStringName = "PictureLikerDB";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -31,8 +30,10 @@ namespace PictureLiker
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<PictureLikerContext>(o => o.UseSqlServer(Configuration.GetConnectionString(DefaultConnectionStringName)));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
