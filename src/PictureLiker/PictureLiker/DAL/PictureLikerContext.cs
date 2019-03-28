d4d4987fcf9bae6474ddbadc52fbbc1ca8dd5fd1
Andy <andy.hsu.job@gmail.com>
2019-03-28 15:44:33 +1000

    Added DBContext

---
+++ src/PictureLiker/PictureLiker/DAL/PictureLikerContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace PictureLiker.DAL
+{
+    public class PictureLikerContext : DbContext
+    {
+        public PictureLikerContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
