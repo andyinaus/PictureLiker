725a5b449b347cfee5651182cdd269777d6dde60
Andy <andy.hsu.job@gmail.com>
2019-03-29 12:16:55 +1000

    Added EqualsIgnoreCase string extension method

---
+++ src/PictureLiker/PictureLiker/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PictureLiker.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool EqualsIgnoreCase(this string value, string valueToCompare)
+        {
+            return value.Equals(valueToCompare, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
