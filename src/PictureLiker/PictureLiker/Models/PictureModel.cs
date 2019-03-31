b01d7ac5453316ce0ecebabd9d005ffba267a782
Andy <andy.hsu.job@gmail.com>
2019-03-31 15:47:27 +1000

    Specified Range attribute on Id property

--- src/PictureLiker/PictureLiker/Models/PictureModel.cs
+++ src/PictureLiker/PictureLiker/Models/PictureModel.cs
@@ -1,7 +1,10 @@
-﻿namespace PictureLiker.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PictureLiker.Models
 {
     public class PictureModel
     {
+        [Range(0, int.MaxValue)]
         public int Id { get; set; }
 
         public byte[] PictureBytes { get; set; }
