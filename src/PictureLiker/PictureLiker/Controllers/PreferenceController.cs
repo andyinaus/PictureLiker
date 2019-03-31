77891a5d6d635ac282155e0a2d362fba973571db
Andy <andy.hsu.job@gmail.com>
2019-03-31 12:38:42 +1000

    Added "Preferences History" feature

---
+++ src/PictureLiker/PictureLiker/Controllers/PreferenceController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PictureLiker.DAL;
+using PictureLiker.Extensions;
+using PictureLiker.Models;
+
+namespace PictureLiker.Controllers
+{
+    public class PreferenceController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PreferenceController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Index()
+        {
+            //TODO: pagination 
+            var preferences = _unitOfWork.PreferenceRepository.TakeAtPage(1, 10, p => p.UserId == User.GetUserId());
+
+            var preferencesWithPicture = preferences.ToList().Select(p => new PicturePreference
+            {
+                PictureId = p.PictureId,
+                IsLiked = p.IsLiked,
+                PictureBytes = _unitOfWork.PictureRepository.FirstOrDefault(pic => pic.Id == p.PictureId)?.Bytes
+            }).ToList();
+
+            return View(new PreferencesModel
+            {
+                Preferences = preferencesWithPicture
+            });
+        }
+    }
+}
---
+++ src/PictureLiker/PictureLiker/Models/PreferencesModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace PictureLiker.Models
+{
+    public class PreferencesModel
+    {
+        public IList<PicturePreference> Preferences { get; set; }
+    }
+
+    public class PicturePreference
+    {
+        public int PictureId { get; set; }
+        public bool IsLiked { get; set; }
+        public byte[] PictureBytes { get; set; }
+    }
+}
