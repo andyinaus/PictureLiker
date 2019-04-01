a3982a1d172f1e679238a4f7158fd92ff51e0990
Andy <andy.hsu.job@gmail.com>
2019-04-01 11:07:35 +1000

    Added todo comment for logging configuration

--- src/PictureLiker/PictureLiker/Startup.cs
+++ src/PictureLiker/PictureLiker/Startup.cs
@@ -14,6 +14,7 @@ using PictureLiker.Services;
 
 namespace PictureLiker
 {
+    //TODO: Configure logging
     public class Startup
     {
         public const string DefaultAuthenticationScheme = CookieAuthenticationDefaults.AuthenticationScheme;
